3428944374392e799443acf816c13b57166e1d0f
ARADHYA200 <aradhyaagarwal2k131987@gmail.com>
2026-02-22 19:51:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 
