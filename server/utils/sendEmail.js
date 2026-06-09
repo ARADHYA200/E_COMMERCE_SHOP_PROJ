4f5bd93ca105c827c13b099c1b080a8729e37191
ARADHYA200 <aradhyaagarwal2k131987@gmail.com>
2026-06-09 22:42:01 +0530

    email done

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,19 +2,23 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  const info = await transporter.sendMail({
+    from: '"E-Commerce Shop" <aradhyaagarwal2k131987@gmail.com>',
     to,
     subject,
     html,
   });
+
+  console.log("Email Sent:", info.messageId);
 };
 
 export default sendEmail;
